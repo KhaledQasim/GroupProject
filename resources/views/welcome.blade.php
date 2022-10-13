37a82a50de4c3b6134ba696f9ce2b4499cbb79ee
Khaled Qasim <210169438@aston.ac.uk>
2022-10-13 21:03:39 +0100

    t

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -35,10 +35,7 @@
                     @endauth
                 </div>
             @endif
-            <div>
-                <h1>edit 1</h1>
 
-             </div>
 
             <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
                 <div class="flex justify-center pt-8 sm:justify-start sm:pt-0">
